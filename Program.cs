e6f6f7fca003ddc3b8fd73256e9b5bda8039592c
Mohd Aatif Khan <ak969049@gmail.com>
2024-11-06 10:08:39 +0530

    After Adding thread details

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 ﻿using ConAppThreadingEx;
+using System.ComponentModel;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 Thread thread = new Thread(new ThreadStart(OurClass.Display));
+Console.WriteLine("Thread Details");
+Console.WriteLine("Background:\t"+thread.IsBackground);
+Console.WriteLine("Name:\t"+thread.Name);
+Console.WriteLine("State:\t"+thread.ThreadState);
+Console.WriteLine("Is Alive:\t"+thread.IsAlive);
 thread.Start();
