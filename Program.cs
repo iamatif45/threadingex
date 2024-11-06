be26e34e49718240bf28a81cc84624415c69abc7
Mohd Aatif Khan <ak969049@gmail.com>
2024-11-06 09:57:07 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ConAppThreadingEx;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Thread thread = new Thread(new ThreadStart(OurClass.Display));
+thread.Start();
